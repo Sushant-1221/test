b07bfea7969c30b7666e2cc3f6060cab03a0b2f7
Your Name <sushantadhav659@gmail.com>
2022-01-05 17:26:17 +0000

    Cal programme added

---
+++ Calc.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Calc {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter number 1 : ");
+		int i = sc.nextInt();
+		System.out.print("Enter number 2 : ");
+		int j = sc.nextInt();
+		System.out.println("1. Add");
+		System.out.println("2. Sub");
+		System.out.println("3. Mult");
+		System.out.println("4. Div");
+
+		System.out.println(" Enter Choice : ");
+		int ch = sc.nextInt();
+		switch (ch) {
+		case 1: // Add
+			break;
+		case 2: // Sub
+			break;
+		case 3: // Mult
+			break;
+		case 4: // Div
+			break;
+
+		}
+	}
+}
